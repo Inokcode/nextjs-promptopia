c4a1672c6c0f060bae64ab11c527457ec17c92f2
Inokcode <regilearn@gmail.com>
2024-05-26 12:55:20 +0530

    provider added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 import '@styles/globals.css';
 
 
@@ -11,6 +12,7 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
@@ -19,6 +21,7 @@ const RootLayout = ({children}) => {
                 <Nav/>
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
  
